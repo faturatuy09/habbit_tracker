4cfc3e331154ee0714d40d68282533113099e09d
faturatuy09 <101823784+faturatuy09@users.noreply.github.com>
2023-06-30 20:21:45 +0700

    Update note.dart

--- mood_tracker/lib/pages/note.dart
+++ mood_tracker/lib/pages/note.dart
@@ -1,130 +1,84 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:mood_tracker/pages/profile.dart';
+import 'package:mood_tracker/pages/home.dart';
+import 'package:mood_tracker/pages/note.dart';
 
-void main() => runApp(MaterialApp(
-  home: MyApp(),
-  debugShowCheckedModeBanner: false,
-));
+void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   @override
-  _MyAppState createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Habit Tracker'),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  //buat type data selectDate dan selectedTime
+  final String title;
 
-  DateTime selectedDate = DateTime.now();
-  TimeOfDay selectedTime = TimeOfDay.now();
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
 
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
 
-  //fungsi untuk mengload Data pada tanggal
-
-  Future<Null> _selectedDate(BuildContext context) async {
-    final DateTime picked = await showDatePicker(
-        context: context,
-        initialDate: selectedDate,
-        firstDate: selectedDate.subtract(Duration(days: 30)),
-        lastDate: DateTime(selectedDate.year + 1));
-    if (picked != null && picked != selectedDate) {
-      setState(() {
-        selectedDate = picked;
-      });
-    }
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(vsync: this, length: 3);
   }
 
-  //fungsi untuk mengload Data pada waktu
-
-  Future<Null> _selectedTime(BuildContext context) async {
-    final TimeOfDay picked_time = await showTimePicker(
-        context: context,
-        initialTime: selectedTime,
-        builder: (BuildContext context, Widget child) {
-          return MediaQuery(
-            data: MediaQuery.of(context).copyWith(alwaysUse24HourFormat: true),
-            child: child,
-          );
-        });
-
-    if (picked_time != null && picked_time != selectedTime)
-      setState(() {
-        selectedTime = picked_time;
-      });
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
   }
-  // Kemudian pada Widget Build, Widget ini berfungsi untuk menampilkan informasi
-  // struktur dasar material design pada halaman, pada Body terdapat Padding Column dan Row.
+
+  void _navigateToPage(int index) {
+    _tabController.animateTo(index);
+  }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
       appBar: AppBar(
-        title: Text('Date & Time Picker'),
-        centerTitle: true,
+        title: Text(widget.title),
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.only(left: 10, right: 10, top: 10),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Text(
-                    "${selectedDate.toLocal()}".split(' ')[0],
-                    style: TextStyle(
-                      fontSize: 20,
-                      foreground: Paint()
-                        ..style = PaintingStyle.stroke
-                        ..strokeWidth = 1
-                        ..color = Colors.blue[700],
-                    ),
-                  ),
-                  SizedBox(width: 10,),
-                  Text("${selectedTime.format(context)}",
-                    style: TextStyle(
-                      fontSize: 20,
-                      foreground: Paint()
-                        ..style = PaintingStyle.stroke
-                        ..strokeWidth = 1
-                        ..color = Colors.blue[700],
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 50,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-
-                  RaisedButton(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(5)
-                    ),
-                    color: Colors.blue,
-                    textColor: Colors.white,
-                    onPressed: () => _selectedDate(context),
-                    child: Text('Selected Date'),
-                  ),
-                  SizedBox(width: 10,),
-                  RaisedButton(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(5)
-                    ),
-                    color: Colors.blue,
-                    textColor: Colors.white,
-                    onPressed: () => _selectedTime(context),
-                    child: Text('Selected Time'),
-                  ),
-                ],
-              )
-            ],
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          HomePage(),
+          NotePage(),
+          ProfilePage(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _navigateToPage,
+        currentIndex: _tabController.index,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
           ),
-        ),
-      ),
-    );
-  }
+          BottomNavigationBarItem(
+            icon: Icon(Icons.note),
+            label: 'Note',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
 }
