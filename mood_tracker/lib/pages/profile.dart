2fa91a394052e63bba7b052e46eb66e5a907b9c2
faridnaufal1 <45188678+faridnaufal1@users.noreply.github.com>
2023-06-30 19:41:23 +0700

    profile.dart

--- mood_tracker/lib/pages/profile.dart
+++ mood_tracker/lib/pages/profile.dart
@@ -3,22 +3,61 @@ import 'package:flutter/material.dart';
 class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(
-            Icons.person,
-            size: 100,
-            color: Colors.blue,
-          ),
-          SizedBox(height: 16),
-          Text(
-            'Profile Page',
-            style: TextStyle(fontSize: 24),
-          ),
-        ],
-      ),
+     return MaterialApp(
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          color: Colors.deepPurple
+        )
+        ),
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("Profile UI"),          
+        ),
+        body: Column(
+          children: <Widget>[
+            CircleAvatar(
+              radius: 80,
+              backgroundImage: AssetImage("images/mfaridnaufal.jpg"),
+            ),
+
+            
+            ListTile(
+              title: Center(child: Text("M Farid Naufal", style: TextStyle(fontSize: 20),),) ,
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                RaisedButton(
+              onPressed: (){
+
+              },
+              child: Text("Message"),
+              ),
+
+              RaisedButton(
+              onPressed: (){
+
+              },
+              child: Text("Hire Me"),
+              ),
+              ],
+            ),
+
+              ListTile(
+                title: Text("About Me", style: TextStyle(fontSize: 20)),
+              )
+
+          ],
+        )
+        
+        
+         ),
+
     );
   }
+  
+  RaisedButton({required Null Function() onPressed, required Text child}) {}
 }
+
